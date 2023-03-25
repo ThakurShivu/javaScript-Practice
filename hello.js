6537c280dd44e231ac9598dfd5197887b2453333
ThakurShivu <93599264+ThakurShivu@users.noreply.github.com>
2023-03-25 12:11:31 +0530

    added new function greeting

--- hello.js
+++ hello.js
@@ -1,4 +1,9 @@
+//const greeting = require("./method")
+
+var method = require("./method.js")
 console.log("Hello this is my first program")
 
 console.log("written in another branch")
 console.log("This is the code")
+
+method.greeeting("Shivani")
---
+++ method.js
@@ -0,0 +1,5 @@
+greet = function greeting(userName){
+    console.log("Welcome ",userName)
+}
+
+module.exports.greeeting =  greet
