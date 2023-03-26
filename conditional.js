3025e8a29a7d34a180092a07897c5d6822c599c0
ThakurShivu <93599264+ThakurShivu@users.noreply.github.com>
2023-03-26 15:37:33 +0530

    added conditional statements

---
+++ conditional.js
@@ -0,0 +1,7 @@
+const DAY='Sunday'
+let SundayMenu='Aalu ka paratha'
+let MondayMenu='Frenchfries'
+if (!DAY){
+    console.log("Today menu is  " +  SundayMenu)
+}else
+console.log("Today menu is" + MondayMenu)
