39865cebe54e9af37c6e719e7f0a917ff003b006
ThakurShivu <93599264+ThakurShivu@users.noreply.github.com>
2023-03-26 15:57:36 +0530

    added code to check condition using if else

--- conditional.js
+++ conditional.js
@@ -1,7 +1,39 @@
-const DAY='Sunday'
-let SundayMenu='Aalu ka paratha'
-let MondayMenu='Frenchfries'
-if (!DAY){
-    console.log("Today menu is  " +  SundayMenu)
-}else
-console.log("Today menu is" + MondayMenu)
+const TODAY='Sunday'
+const SUNDAY_MENU='Aalu ka paratha'
+const MONDAY_MENU='Coffee'
+const TUESDAY_MENU='Frenchfries'
+const WEDNESDAY_MENU='Momos'
+const THURSDAY_MENU='Noodles'
+const FRIDAY_MENU='Chhole'
+const SATURDAY_MENU='Rajma'
+//days
+const SUNDAY = "Sunday"
+const MONDAY = "Monday"
+const TUESDAY = "Tuesday"
+const WEDNESDA = "Wednesday"
+const THURSDAY = "Thursday"
+const FRIDAY = "Friday"
+const SATURDAY = "Saturday" 
+
+if (TODAY == SUNDAY){
+    console.log("Today is : "+TODAY)
+    console.log("Today's menu is : "+ SUNDAY_MENU)
+}else if (TODAY == MONDAY){
+    console.log("Today is : "+TODAY)
+    console.log("Today's menu is : "+ MONDAY_MENU)
+}else if (TODAY == TUESDAY){
+    console.log("Today is : "+TODAY)
+    console.log("Today's menu is : "+ TUESDAY_MENU)
+}else if (TODAY == WEDNESDAY){
+    console.log("Today is : "+TODAY)
+    console.log("Today's menu is : "+ WEDNESDAY_MENU)
+}else if (TODAY == THURSDAY){
+    console.log("Today is : "+TODAY)
+    console.log("Today's menu is : "+ THURSDAY_MENU)
+}else if (TODAY == FRIDAY){
+    console.log("Today is : "+TODAY)
+    console.log("Today's menu is : "+ FRIDAY_MENU)
+}else{
+    console.log("Today is : "+TODAY)
+    console.log("Today's menu is : "+ SATURDAY_MENU)
+}
