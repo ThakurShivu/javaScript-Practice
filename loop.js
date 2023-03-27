f96fe5cd11da66aa3f434915fd3ca366a244b2fe
ThakurShivu <93599264+ThakurShivu@users.noreply.github.com>
2023-03-27 22:03:40 +0530

    create a star pattern using loop

--- loop.js
+++ loop.js
@@ -28,11 +28,56 @@ for (let i = 1; i <= 10; i++) {
 
 //print all even 1 to 20 using loop
 console.log("All even 0 to 20 are following given bellow")
-for (let i = 0; i <= 20; i++) {
+for (let i = 0; i <=50; i++) {
     reminder = i% 2
     if (reminder > 0) {
         continue
     } else {
         console.log(i," ")
     }
+}
+
+console.log("Infinite loop started..................")
+// for(let i=0;;i++){
+//     console.log("Hey",i)
+
+// }
+
+//WHILE loop using for loop
+let counter = 0
+for(;counter < 10;){
+   var name = "Shivani"
+   console.log("My name is",name)
+   counter++
+}
+//DO-WHILE loop using for loop
+for (; ;) {
+    var name = "Shivani"
+    console.log("My name is", name)
+
+    if (counter < 10) {
+        counter++
+    } else {
+        break
+    }
+}
+//nested for loop 
+for (let i = 0; i<10;i++){
+    for (let j =1;j<=5;j++){
+        process.stdout.write(""+j)
+    }
+    console.log()
+}
+
+//write a program to print a start(*) pattern 
+row=5
+colum = 5
+for(row=0;row<5;row++){
+    for(colum=4;colum>=0;colum--){
+        if (colum<=row)
+            process.stdout.write("*")
+        else
+            process.stdout.write(" ")
+    }
+    console.log()
 }
