44baa42987b87850c84a5b60045b5ebcf9d87f62
ThakurShivu <93599264+ThakurShivu@users.noreply.github.com>
2023-03-30 19:22:03 +0530

    two numbers calculated using function

--- Function.js
+++ Function.js
@@ -16,28 +16,61 @@ const FRIDAY = "Friday"
 const SATURDAY = "Saturday"
 
 
-function BreakfastMenuByDay()
-
-if (TODAY == SUNDAY) {
-    console.log("Today is : " + TODAY)
-    console.log("Today's menu is : " + SUNDAY_MENU)
-} else if (TODAY == MONDAY) {
-    console.log("Today is : " + TODAY)
-    console.log("Today's menu is : " + MONDAY_MENU)
-} else if (TODAY == TUESDAY) {
-    console.log("Today is : " + TODAY)
-    console.log("Today's menu is : " + TUESDAY_MENU)
-} else if (TODAY == WEDNESDAY) {
-    console.log("Today is : " + TODAY)
-    console.log("Today's menu is : " + WEDNESDAY_MENU)
-} else if (TODAY == THURSDAY) {
-    console.log("Today is : " + TODAY)
-    console.log("Today's menu is : " + THURSDAY_MENU)
-} else if (TODAY == FRIDAY) {
-    console.log("Today is : " + TODAY)
-    console.log("Today's menu is : " + FRIDAY_MENU)
-} else {
-    console.log("Today is : " + TODAY)
-    console.log("Today's menu is : " + SATURDAY_MENU)
+function BreakfastMenuByDay(TODAY) {
+    let menu 
+    if (TODAY == SUNDAY) {
+        menu = SUNDAY_MENU
+    } else if (TODAY == MONDAY) {
+        menu = MONDAY_MENU
+    } else if (TODAY == TUESDAY) {
+        menu = TUESDAY_MENU
+    } else if (TODAY == WEDNESDAY) {
+        menu =WNDESDAY_MENU
+    } else if (TODAY == THURSDAY) {
+        menu =THURSDAY_MENU 
+    } else if (TODAY == FRIDAY) {
+        menu =FRIDAY_MENU
+    } else {
+        menu = SATERDAY_MENU
+    }
+    return `Today is : ${TODAY} and Today's menu is: ${menu}`
 }
-return(BreakfastMenuByDay)
+console.log(BreakfastMenuByDay(MONDAY))
+
+//write a function to return additon of two numbers  => add
+//write a function to return subtract of two numbers => sub
+//write a function to return multiply of two numbers = mult
+//write a function to return devid of two numbers = div
+//write a function to return reminder of a/b = mod 
+
+//write a function to return additon of two numbers  => add
+function add(num1 , num2){
+    return  num1 + num2
+}
+console.log(add(4,5))
+
+
+//write a function to return subtract of two numbers => sub
+function sub(num1 ,num2){
+    return num1 - num2
+}
+console.log(sub(3,2))
+
+//write a function to return subtract of two numbers => mult
+function mult(num1,num2){
+    return num1*num2
+}
+console.log(mult(7,8))
+
+
+//write a function to return subtract of two numbers => div
+function div(num1 ,num2){
+    return num1/num2
+}
+console.log(div(9,5))
+
+//write a function to return subtract of two numbers => mod
+function mod(num1,num2){
+    return num1%num2
+}
+console.log(mod(5,7))
