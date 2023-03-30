38e7e36b3ae2622f4a32fc56b991f44ac13049ad
ThakurShivu <93599264+ThakurShivu@users.noreply.github.com>
2023-03-30 16:49:40 +0530

    Functions in javascript.

---
+++ Function.js
@@ -0,0 +1,43 @@
+const TODAY = 'Wednesday'
+const SUNDAY_MENU = 'Aalu ka paratha'
+const MONDAY_MENU = 'Coffee'
+const TUESDAY_MENU = 'Frenchfries'
+const WEDNESDAY_MENU = 'Momos'
+const THURSDAY_MENU = 'Noodles'
+const FRIDAY_MENU = 'Chhole'
+const SATURDAY_MENU = 'Rajma'
+//days
+const SUNDAY = "Sunday"
+const MONDAY = "Monday"
+const TUESDAY = "Tuesday"
+const WEDNESDAY = "Wednesday"
+const THURSDAY = "Thursday"
+const FRIDAY = "Friday"
+const SATURDAY = "Saturday"
+
+
+function BreakfastMenuByDay()
+
+if (TODAY == SUNDAY) {
+    console.log("Today is : " + TODAY)
+    console.log("Today's menu is : " + SUNDAY_MENU)
+} else if (TODAY == MONDAY) {
+    console.log("Today is : " + TODAY)
+    console.log("Today's menu is : " + MONDAY_MENU)
+} else if (TODAY == TUESDAY) {
+    console.log("Today is : " + TODAY)
+    console.log("Today's menu is : " + TUESDAY_MENU)
+} else if (TODAY == WEDNESDAY) {
+    console.log("Today is : " + TODAY)
+    console.log("Today's menu is : " + WEDNESDAY_MENU)
+} else if (TODAY == THURSDAY) {
+    console.log("Today is : " + TODAY)
+    console.log("Today's menu is : " + THURSDAY_MENU)
+} else if (TODAY == FRIDAY) {
+    console.log("Today is : " + TODAY)
+    console.log("Today's menu is : " + FRIDAY_MENU)
+} else {
+    console.log("Today is : " + TODAY)
+    console.log("Today's menu is : " + SATURDAY_MENU)
+}
+return(BreakfastMenuByDay)
